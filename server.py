d2f67c943462283d45c3009d9bf43bbb15b66240
Your Name <you@example.com>
2020-04-25 13:52:13 +0800

    support http and file

--- server.py
+++ server.py
@@ -19,17 +19,23 @@ def image():
     try:
         json = request.get_json()
         url = json['url']
+
         # 1.下载图片
-        file = os.path.join(os.path.dirname(__file__), "tmp", str(uuid.uuid4()) + "_captcha.jpg")
-        _, code = download_image_as_jpeg(url, file)
-        if not code == 200:
-            return make_response(jsonify({'message': "解析失败Code:" + code}), 400)
+        if url.startswith('http'):
+            file = os.path.join(os.path.dirname(__file__), "tmp", str(uuid.uuid4()) + "_captcha.jpg")
+            _, code = download_image_as_jpeg(url, file)
+            if not code == 200:
+                return make_response(jsonify({'message': "解析失败Code:" + code}), 400)
+        else:
+            file = url
+
         # 2.识别图片
         res = crack_qq.qq_mark_pos(file)
         dis = res.x.values[0]
         tacks = crack_qq.get_track_list(dis)  # 模拟加速度
         app.logger.debug("解析成功,需要移动距离:{},点数:{}".format(dis, len(tacks)))
-        os.remove(file)  # 解析后删除文件就可以了
+        if url.startswith('http'):
+            os.remove(file)  # 解析后删除文件就可以了
         return make_response(jsonify({'message': "解析成功", 'data': {'x': dis, 'list': tacks, 'url': url}}), 200)
     except Exception as e:
         print("e：", e)
